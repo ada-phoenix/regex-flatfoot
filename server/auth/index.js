7d35b085d3a1458c3660005171193ae484b12a93
MikaelaSheldt <msheldt@gmail.com>
2019-03-18 18:38:07 -0400

    BUILD(auth/index): implimented getGames, send gamesVisted with user data (#7)

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,10 +1,13 @@
 const router = require('express').Router()
 const User = require('../db/models/user')
+const Game = require('../db/models/game')
 module.exports = router
 
 router.post('/login', async (req, res, next) => {
   try {
-    const user = await User.findOne({where: {email: req.body.email}})
+    const user = await User.findOne({
+      where: {email: req.body.email}
+    })
     if (!user) {
       console.log('No such user found:', req.body.email)
       res.status(401).send('Wrong username and/or password')
@@ -12,7 +15,17 @@ router.post('/login', async (req, res, next) => {
       console.log('Incorrect password for user:', req.body.email)
       res.status(401).send('Wrong username and/or password')
     } else {
-      req.login(user, err => (err ? next(err) : res.json(user)))
+      const gamesVisted = await user.getGames({raw: true})
+      const userData = {
+        email: user.email,
+        name: user.name,
+        level: user.level,
+        levelstage: user.levelstage,
+        id: user.id,
+        gamesVisted
+      }
+      console.log('GAMES_VISTED', userData)
+      req.login(user, err => (err ? next(err) : res.json(userData)))
     }
   } catch (err) {
     next(err)
