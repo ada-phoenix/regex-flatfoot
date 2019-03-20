73a39e242a557bbc89e8febd71081a8a8332bc68
Mikaela Sheldt <msheldt@gmail.com>
2019-03-20 16:28:52 -0400

    BUILD(store/cluster.js): built cluster store

---
+++ client/store/cluster.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+// INITIAL STATE
+const defaultCluster = {}
+
+// ACTION TYPES
+const GOT_CLUSTER = 'GOT_CLUSTER'
+
+// ACTION CREATORS
+const gotCluster = cluster => ({
+  type: GOT_CLUSTER,
+  cluster
+})
+
+// THUNK CREATORS
+export const getCluster = clusterId => async dispatch => {
+  const {data} = await axios.get(`/api/cluster/${clusterId}`)
+  dispatch(gotCluster(data))
+}
+
+// REDUCER
+export default function(state = defaultCluster, action) {
+  switch (action.type) {
+    case GOT_CLUSTER: {
+      return action.cluster
+    }
+    default:
+      return state
+  }
+}
