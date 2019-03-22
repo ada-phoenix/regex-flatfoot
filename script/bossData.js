4727e3a56f82164efe6057b6a7383e4f118b1837
Mikaela Sheldt <msheldt@gmail.com>
2019-03-22 11:52:33 -0400

    DATA(bossData): added boss data

---
+++ script/bossData.js
@@ -0,0 +1,19 @@
+bossData = [
+  {
+    haystack:
+      "This is a very tricky string isn't it? We're talking about String Theory or something here!",
+    needle: ['string', 'String'],
+    question:
+      "find every instance of 'string' in the folowing message upper and lowercase.",
+    timer: 30000,
+    story:
+      'Good thing for me I have a failsafe. A bomb! Unless you can decode the instructions for disarming the bomb within 60 seconds it will explode and I will make my escape!',
+    picture:
+      'https://i.pinimg.com/236x/f8/75/7f/f8757f1aae1b4dc7a3eeced04eb51c94--men-portrait-bowties.jpg',
+    correct: "I'm text telling you how smart you are",
+    incorrect: "I'm text telling you to try harder next time.",
+    clusterId: 1
+  }
+]
+
+module.exports = bossData
