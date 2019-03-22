65c32b5a649226c9cb49f43fe1937bdd5b32b903
Alex <Jillbk22@gmail.com>
2019-03-22 10:58:50 -0400

    FIX(Components/boss): changed time limit

--- client/components/Boss.js
+++ client/components/Boss.js
@@ -11,9 +11,9 @@ class Boss extends React.Component {
       needle: ['string', 'String'],
       question:
         "find every instance of 'string' in the folowing message upper and lowercase.",
-      timer: 60000,
+      timer: 30000,
       story:
-        'Good thing for me I have a failsafe. A bomb! Unless you can decode the instructions for disarming the bomb within 60 seconds it will explode and I will make my escape!',
+        'Good thing for me I have a failsafe. A bomb! Unless you can decode the instructions for disarming the bomb within 30 seconds it will explode and I will make my escape!',
       picture:
         'https://i.pinimg.com/236x/f8/75/7f/f8757f1aae1b4dc7a3eeced04eb51c94--men-portrait-bowties.jpg',
       result: [''],
