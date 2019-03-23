7c45cbb956375fc01a85181d47650ddb5f246830
Sara Parelhoff <sara.parelhoff@gmail.com>
2019-03-23 13:19:15 -0400

    styling-start

--- client/index.js
+++ client/index.js
@@ -5,15 +5,37 @@ import {Router} from 'react-router-dom'
 import history from './history'
 import store from './store'
 import App from './app'
+import {createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#cfd8dc',
+      light: '#ffffff',
+      dark: '#9ea7aa'
+    },
+    secondary: {
+      main: '#7cb342',
+      light: '#aee571',
+      dark: '#4b830d'
+    },
+    text: {
+      primary: '#212121',
+      secondary: '#fafafa'
+    }
+  }
+})
 
 // establishes socket connection
 import './socket'
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={history}>
-      <App />
-    </Router>
+    <MuiThemeProvider theme={theme}>
+      <Router history={history}>
+        <App />
+      </Router>
+    </MuiThemeProvider>
   </Provider>,
   document.getElementById('app')
 )
