be9c78f0aab1ea58a7a5c5cc15e8c1ab9f76e020
Mikaela Sheldt <msheldt@gmail.com>
2019-03-25 07:36:57 -0400

    FIX(component/boss): illiminate unnesesarry state properties

--- client/components/Boss.js
+++ client/components/Boss.js
@@ -14,19 +14,9 @@ class Boss extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      haystack:
-        "This is a very tricky string isn't it? We're talking about String Theory or something here!",
-      needle: ['string', 'String'],
-      question:
-        "find every instance of 'string' in the folowing message upper and lowercase.",
-      story:
-        "You've found me you scoundrel!\nGood thing for me I have a failsafe. A bomb! Unless you can decode the instructions for disarming the bomb within 30 seconds it will explode and I will make my escape!",
-      picture:
-        'https://i.pinimg.com/236x/f8/75/7f/f8757f1aae1b4dc7a3eeced04eb51c94--men-portrait-bowties.jpg',
       result: [''],
       preview: [''],
       elapsed: 0,
-      time: 30000,
       start: new Date(),
       input: ''
     }
