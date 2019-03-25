3fb4ebb711fa78d8c6ef0ab8a02b3572bd410654
Sara Parelhoff <sara.parelhoff@gmail.com>
2019-03-25 00:45:18 -0400

    STYLE(QuestionScreen, Problem): grid in place

--- client/components/ConsoleIcon.js
+++ client/components/ConsoleIcon.js
@@ -2,29 +2,30 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {withStyles} from '@material-ui/core/styles'
 import Avatar from '@material-ui/core/Avatar'
-import Button from '@material-ui/core/Button'
 import Card from '@material-ui/core/Card'
-import CardActionArea from '@material-ui/core/CardActionArea'
 import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
-import CardMedia from '@material-ui/core/CardMedia'
-
 import NotesIcon from './NotesIcon'
 import LessonIcon from './LessonIcon'
 import CasefileIcon from './CasefileIcon'
 
 const styles = {
   card: {
-    maxWidth: 600
+    maxWidth: 250,
+    border: '4mm groove #424242',
+    borderRadius: 5,
+    background: 'linear-gradient(45deg, #1b1b1b, #6d6d6d)',
+    backgroundColor: '#000000'
   },
   media: {
-    height: 250
+    height: 0,
+    paddingTop: '56.25%'
   },
   bigAvatar: {
-    margin: 10,
-    width: 200,
-    height: 200,
-    borderRadius: 0
+    margin: 0,
+    width: '100%',
+    height: '100%',
+    borderRadius: 0,
+    border: '10px solid #212121'
   }
 }
 
--- client/components/QuestionScreen.js
+++ client/components/QuestionScreen.js
@@ -4,10 +4,10 @@ import {connect} from 'react-redux'
 import Prompt from './Prompt'
 import Problem from './Problem'
 import {getGame} from '../store/game'
-
-import Console from './Console'
-import PaperConsole from './PaperConsole'
 import ConsoleIcon from './ConsoleIcon'
+import Grid from '@material-ui/core/Grid'
+import withStyles from '@material-ui/core/styles/withStyles'
+import Typography from '@material-ui/core/Typography'
 
 /**
  * COMPONENT
@@ -34,15 +34,58 @@ class QuestionScreen extends React.Component {
   }
 
   render() {
-    // console.log('question screen props ', this.props)
+    const {classes} = this.props
     return this.state.gotGame ? (
       <div>
-        <div className="container">
-          <Console />
-          <PaperConsole />
-          <ConsoleIcon />
-        </div>
-        <Problem history={this.props.history} />
+        <Grid
+          container
+          wrap="wrap"
+          spacing={24}
+          alignItems="flex-start"
+          className={classes.consoleGrid}
+        >
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md="auto"
+            lg={3}
+            className={classes.consoleGridItem}
+          >
+            <ConsoleIcon />
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md="auto"
+            lg={9}
+            className={classes.consoleGridItem}
+          >
+            <Typography
+              paragraph={true}
+              headlingMapping="p"
+              className={classes.lessonText}
+            >
+              {this.props.game.lesson}
+            </Typography>
+            <Typography
+              paragraph={true}
+              headlingMapping="p"
+              className={classes.lessonText}
+            >
+              {this.props.game.question}
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid
+          container
+          wrap="wrap"
+          spacing={24}
+          className={classes.consoleGrid}
+        >
+          <Problem history={this.props.history} />
+        </Grid>
       </div>
     ) : (
       <div>
@@ -54,6 +97,27 @@ class QuestionScreen extends React.Component {
     )
   }
 }
+
+//STYLES
+const styles = theme => ({
+  consoleGrid: {
+    backgroundColor: 'pink',
+    padding: '1%',
+    border: '4mm groove #424242',
+    borderRadius: 5
+  },
+  consoleGridItem: {
+    padding: '2%',
+    backgroundColor: '#000000',
+    border: '4mm groove #424242',
+    justifySelf: 'center'
+  },
+  lessonText: {
+    fontFamily: 'Cutive',
+    fontSize: '1em',
+    color: '#FFFFFF'
+  }
+})
 
 /**
  * CONTAINER
@@ -73,7 +137,9 @@ const mapDispatch = dispatch => {
   }
 }
 
-export default connect(mapState, mapDispatch)(QuestionScreen)
+export default connect(mapState, mapDispatch)(
+  withStyles(styles)(QuestionScreen)
+)
 
 /**
  * PROP TYPES
