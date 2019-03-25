f54cf6517e4f9d97ce86ccbcc88ade4be91af422
Mikaela Sheldt <msheldt@gmail.com>
2019-03-25 13:00:31 -0400

    FEAT(casefileIcon): toggle green badge with new clues

--- client/store/index.js
+++ client/store/index.js
@@ -5,8 +5,9 @@ import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
 import game from './game'
 import cluster from './cluster'
+import effects from './effects'
 
-const reducer = combineReducers({user, game, cluster})
+const reducer = combineReducers({user, game, cluster, effects})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
