3a8d14ba1ba03f0e281596353072e2ad7c19175c
Alex <Jillbk22@gmail.com>
2019-03-25 12:49:51 -0400

    merged

---
+++ client/components/NoMore.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import Button from '@material-ui/core/Button'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+import {withStyles} from '@material-ui/core'
+
+const styles = theme => ({
+  root: {
+    margin: 50,
+    padding: 50,
+    maxWidth: 700,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: theme.palette.primary.light
+  },
+  button: {
+    margin: 50,
+    justifyContent: 'center'
+  },
+  subtitle2: {
+    margin: 25
+  },
+  type: {
+    fontFamily: 'Cutive'
+  },
+  h2: {
+    textAlign: 'center',
+    [theme.breakpoints.down('xs')]: {
+      fontSize: '30px'
+    },
+    [theme.breakpoints.up('sm')]: {
+      fontSize: '50px'
+    },
+    [theme.breakpoints.up('md')]: {
+      fontSize: '70px'
+    }
+  }
+})
+
+class NoMore extends React.Component {
+  render() {
+    const {classes} = this.props
+
+    return (
+      <Paper className={classes.root}>
+        <Typography className={classes.h2} variant="h2">
+          Here we are
+        </Typography>
+        <Button
+          className={classes.button}
+          variant="contained"
+          color="secondary"
+          onClick={() => {
+            this.props.history.push('/boss')
+          }}
+        >
+          Replay!
+        </Button>
+      </Paper>
+    )
+  }
+}
+
+//Container
+export default connect()(withStyles(styles)(NoMore))
