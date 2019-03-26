8882a63a7b9f8def00a9f05d2740e08df7dcddd5
Alex <36085954+Classson@users.noreply.github.com>
2019-03-26 11:50:42 -0400

    Update gameData.js

--- script/gameData.js
+++ script/gameData.js
@@ -34,7 +34,7 @@ const gameData = [
       'Are you using the back slashes? No need to worry about capitalization for now.',
     clue: 'At the parking lot, a seagull winked at me. He knows something.',
     correct:
-      "Good work, Rookie.\nThe boys picked up Dmytrenko and he's singing like a canary! Go checkout the parking lot behind the hotdog stand on Ninth and Grand. He said it's one of their drop off points.",
+      "Good work, Rookie.\nThe boys picked up Dmytrenko and he\'s singing like a canary! Go checkout the parking lot behind the hotdog stand on Ninth and Grand. He said it\'s one of their drop off points.",
     incorrect:
       'He slipped through our fingers again!?\nGet it together Rookie! Grab your self a cup of coffee and head back to the station.',
     sound: '/seagull.wav'
