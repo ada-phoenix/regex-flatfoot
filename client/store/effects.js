840757f3c56e8bbdd6a3ee14a8ce1dc1d8daef4a
Mikaela Sheldt <msheldt@gmail.com>
2019-03-26 15:06:48 -0400

    FEAT(store/effects): notes update on state

--- client/store/effects.js
+++ client/store/effects.js
@@ -1,10 +1,14 @@
 import axios from 'axios'
 
 // INITIAL STATE
-const defaultEffects = {caseFileBadgeInvisible: true}
+const defaultEffects = {
+  caseFileBadgeInvisible: true,
+  notes: []
+}
 
 // ACTION TYPES
 const SET_CASEFILE_BADGE = 'SET_CASEFILE_BADGE'
+const ADD_NOTE = 'ADD_NOTE'
 
 // ACTION CREATORS
 export const setCasefileBadge = boolean => ({
@@ -12,14 +16,20 @@ export const setCasefileBadge = boolean => ({
   boolean
 })
 
+export const addNote = note => ({
+  type: ADD_NOTE,
+  note
+})
+
 // THUNK CREATORS
 
 // REDUCER
 export default function(state = defaultEffects, action) {
   switch (action.type) {
-    case SET_CASEFILE_BADGE: {
+    case SET_CASEFILE_BADGE:
       return {...state, caseFileBadgeInvisible: action.boolean}
-    }
+    case ADD_NOTE:
+      return {...state, notes: [...state.notes, action.note]}
     default:
       return state
   }
