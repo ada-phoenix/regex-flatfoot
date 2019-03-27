3fa61466c0f9a92c4a8d3335c76438767f826ee5
Mikaela Sheldt <msheldt@gmail.com>
2019-03-27 15:14:55 -0400

    FEAT(nav): button to logout

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,16 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {withStyles} from '@material-ui/core/styles'
 import {connect} from 'react-redux'
 import Button from '@material-ui/core/Button'
 import {logout} from '../store'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
+const styles = theme => ({
+  margin: {
+    margin: theme.spacing.unit
+  },
+  extendedIcon: {
+    marginRight: theme.spacing.unit
+  }
+})
+
+const Navbar = ({handleClick, isLoggedIn, classes}) => (
   <nav>
     {isLoggedIn ? (
       <div>
-        <a href="#" onClick={handleClick}>
+        <Button
+          size="small"
+          variant="outlined"
+          className={classes.margin}
+          onClick={handleClick}
+        >
           Logout
-        </a>
+        </Button>
       </div>
     ) : (
       <div />
@@ -35,12 +50,19 @@ const mapDispatch = dispatch => {
   }
 }
 
-export default connect(mapState, mapDispatch)(Navbar)
+export default connect(mapState, mapDispatch)(withStyles(styles)(Navbar))
 
 /**
  * PROP TYPES
  */
 Navbar.propTypes = {
   handleClick: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
+  isLoggedIn: PropTypes.bool.isRequired,
+  classes: PropTypes.object.isRequired
 }
+
+/*
+<a href="#" onClick={handleClick}>
+  Logout
+</a>
+*/
