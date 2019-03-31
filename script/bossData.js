7506e8b78f52a8d0c431baf23ccf3b71c6b3ac38
Alex <36085954+Classson@users.noreply.github.com>
2019-03-31 17:50:55 -0400

    Update bossData.js

--- script/bossData.js
+++ script/bossData.js
@@ -9,7 +9,6 @@ const bossData = [
     story:
       "You found me you scoundrel!\nGood thing for me I have a failsafe. A bomb! Unless you can decode the instructions for disarming the bomb within 30 seconds it will explode and I will make my escape! Oh and one more thing, I've stolen your highlighter.",
     picture:
-      // 'https://i.pinimg.com/236x/f8/75/7f/f8757f1aae1b4dc7a3eeced04eb51c94--men-portrait-bowties.jpg',
       'https://i.pinimg.com/originals/20/df/f0/20dff03269c1ed88e336f25cb45b4f92.jpg',
     correct:
       'Another monster behind bars! But this low level ruffian was just a pawn. Stay on the case, ace!',
